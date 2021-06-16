8e0cb1aa0e50e7dc6d303fb36d0e5c4ba7ea4b4b
msmaltsev <maltsevmark@gmail.com>
2021-06-17 00:36:07 +0300

    not a single case; params passed via start_link

--- spider.erl
+++ spider.erl
@@ -1,27 +1,23 @@
 -module(spider).
--export([start_link/0, stop/1, enable/1, disable/1]).
+-export([start_link/2, stop/1, enable/1, disable/1]).
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2]).
 -behaviour(gen_server).
 
 -record(spider_state, {working_status = disabled, 
                         action = idle, 
+                        url = url,
                         cooldown = 10000}).
 
-% parameters
-url() -> <<"https://lenta.ru/rss/">>.
-cooldown() -> 10 * 1000.
-
 %% behavior
-
-start_link() ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, params, []).
+start_link(Url, Cooldown) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, {Url, Cooldown}, []).
 
 stop(Pid) ->
     gen_server:call(Pid, stop).
 
-init(params) ->
-    {ok, _Timer} = timer:send_after(1000, self(), perform),
-    {ok, #spider_state{working_status = disabled, cooldown = 10000}}.
+init({Url, Cooldown}) ->
+    self() ! perform,
+    {ok, #spider_state{working_status = disabled, url=Url, action=idle, cooldown = Cooldown}}.
 
 code_change(_OldVsn, SpiderState, _Extra) ->
     {ok, SpiderState}.
@@ -32,9 +28,7 @@ terminate(Reason, #spider_state{}) ->
 handle_cast(_Msg, SpiderState) ->
     {noreply, SpiderState}.
 
-
 %% actions
-
 enable(Pid) ->
     gen_server:call(Pid, enable).
 
@@ -42,81 +36,56 @@ disable(Pid) ->
     gen_server:call(Pid, disable).
 
 %% info
-
 handle_info(perform, SpiderState = #spider_state{working_status=disabled}) ->
     self() ! perform,
     {noreply, SpiderState};
 
-
-handle_info(perform, #spider_state{working_status=enabled, cooldown=Ms, action = Action}) ->
-
-    case Action of 
-
-        idle ->
-            io:format("woke up, went to is ~p~n", [url()]),
-            self() ! perform,
-            {noreply, #spider_state{working_status=enabled, cooldown = Ms, action = traverse}};
-
-        traverse ->
-            io:format("traversing~n"),
-            % catch errors - send to failure
+handle_info(perform, #spider_state{working_status=enabled, cooldown=Ms, url=Url, action=idle}) ->
+    
+    io:format("woke up, went to is ~p~n", [Url]),
+    self() ! perform,
+    {noreply, #spider_state{working_status=enabled, cooldown = Ms, url = Url, action = traverse}};
 
 
-            {ok, StatusCode, RespHeaders, ClientRef} = hackney:get(url(), [], <<>>, []),
-            io:format("Status: ~p~n", [StatusCode]),
-            {ok, Body} = hackney:body(ClientRef),
-            io:format(Body),
+handle_info(perform, #spider_state{working_status=enabled, url=Url, cooldown=Ms, action=traverse}) ->
 
-            self() ! perform,
-            {noreply, #spider_state{working_status=enabled, cooldown = Ms, action = success}};
-        success ->
-            io:format("done traversing~n"),
-            io:format("next traverse in ~p~n", [cooldown()]),
-            {ok, _Timer} = timer:send_after(cooldown(), self(), perform),
-            {noreply, #spider_state{working_status=enabled, cooldown = cooldown(), action = idle}}
-    end.
+    io:format("traversing~n"),
+    % catch errors - send to action failure - coming soon, it's almost 1 am, for christ sake, i wanna play wolfenstein
+    io:format("~p~n", [Url]),
+    {ok, StatusCode, RespHeaders, ClientRef} = hackney:request(get, Url, [], <<>>, []),
+    io:format("Status: ~p~n", [StatusCode]),
+    self() ! perform,
+    {noreply, #spider_state{working_status=enabled, cooldown = Ms, url=Url, action = success}};
+
+
+handle_info(perform, #spider_state{working_status=enabled, cooldown=Cooldown, url=Url, action=success}) ->
+
+    io:format("done traversing~n"),
+    io:format("next traverse in ~p~n", [Cooldown]),
+    {ok, _Timer} = timer:send_after(Cooldown, self(), perform),
+    {noreply, #spider_state{working_status=enabled, cooldown = Cooldown, url=Url, action = idle}}.
 
 
 %% calls
-
-handle_call(enable, _From, SpiderState = #spider_state{working_status = WorkingStatus}) ->
-    case WorkingStatus of
-        enabled -> 
-            {reply, {ok, already_enabled}, SpiderState};
-        disabled -> 
-            {reply, {ok, enabled}, #spider_state{working_status=enabled}}
-    end;
+handle_call(enable, _From, SpiderState = #spider_state{working_status = enabled}) ->
+    {reply, {ok, already_enabled}, SpiderState};
 
 
-handle_call(disable, _From, SpiderState = #spider_state{working_status = WorkingStatus, action = Action}) ->
-
-    case Action of 
-        idle ->
-            case WorkingStatus of
-                enabled -> 
-                    {reply, {ok, disabled}, #spider_state{working_status=disabled}};
-                disabled ->
-                    {reply, {ok, already_disabled}, SpiderState}
-            end;
-        _ -> 
-            {reply, {ok, busy}, SpiderState}
-    end;
+handle_call(enable, _From, #spider_state{working_status = disabled, url=Url, cooldown=Cooldown}) ->
+    {reply, {ok, enabled}, #spider_state{working_status=enabled, url=Url, cooldown = Cooldown}};
 
 
-handle_call(stop, _From, SpiderState = #spider_state{working_status = WorkingStatus, action = Action}) ->
+handle_call(disable, _From, #spider_state{working_status = enabled, action = idle, url=Url, cooldown=Cooldown}) ->
+    {reply, {ok, disabled}, #spider_state{working_status=disabled, url=Url, cooldown=Cooldown}};
 
-    case Action of 
 
-        idle ->
+handle_call(disable, _From, SpiderState = #spider_state{working_status = disabled}) ->
+    {reply, {ok, already_disabled}, SpiderState};
 
-            case WorkingStatus of
-                enabled ->
-                    {reply, {ok, cant_stop_enabled}, SpiderState};
-                disabled -> 
-                    {stop, normal, ok, SpiderState}
-            end;
 
-        _ ->
-            {reply, {ok, busy}, SpiderState}
+handle_call(stop, _From, SpiderState = #spider_state{working_status = enabled}) ->
+    {reply, {ok, cant_stop_enabled}, SpiderState};
 
-    end.
+
+handle_call(stop, _From, SpiderState = #spider_state{working_status = disabled}) ->
+    {stop, normal, ok, SpiderState}.
