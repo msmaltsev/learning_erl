89e343028b0b7c7fad78accf4e3563c5e6003b49
msmaltsev <maltsevmark@gmail.com>
2021-06-25 11:39:24 +0300

    xmerl

--- src/spider.erl
+++ src/spider.erl
@@ -1,5 +1,5 @@
 -module(spider).
--export([start_link/2, stop/1, enable/1, disable/1]).
+-export([start_link/2, stop/1, enable/1, disable/1, start_link/0]).
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, terminate/2]).
 -behaviour(gen_server).
 
@@ -8,8 +8,17 @@
                         url = url,
                         cooldown = 10000}).
 
+-include_app("hackney/include/hackney_lib.hrl").
+-include_lib("xmerl/include/xmerl.hrl").
+
 %% behavior
 start_link(Url, Cooldown) ->
+    gen_server:start_link(?MODULE, {Url, Cooldown}, []).
+
+start_link() ->
+    Url = "https://meduza.io/rss2/all",
+    Cooldown = 1000000,
+    io:format("launching with parameters: ~p, ~p~n", [Url, Cooldown]),
     gen_server:start_link(?MODULE, {Url, Cooldown}, []).
 
 stop(Pid) ->
@@ -50,9 +59,35 @@ handle_info(perform, #spider_state{working_status=enabled, cooldown=Ms, url=Url,
 handle_info(perform, #spider_state{working_status=enabled, url=Url, cooldown=Ms, action=traverse}) ->
 
     io:format("image we traversing to ~p~n", [Url]),
-    % catch errors - send to action failure - coming soon, it's almost 1 am, for christ sake, i wanna play wolfenstein
+    {ok, Platform} = maps:find(host, uri_string:parse(Url)),
     {ok, StatusCode, RespHeaders, ClientRef} = hackney:request(get, Url, [], <<>>, []),
     io:format("Status: ~p~n", [StatusCode]),
+    {ok, Body} = hackney:body(ClientRef),
+
+    % Filename = Platform ++ integer_to_list(erlang:system_time()) ++ ".txt",
+    % file:write_file(Filename, [Body]),
+
+    {Xml, _} = xmerl_scan:string(binary_to_list(Body)),
+    % FeedTitle = xmerl_xpath:string("/rss/channel/title/text()", Xml),
+    % io:format("~p~n", [FeedTitle]),
+
+    Items = xmerl_xpath:string("/rss/channel//item", Xml),
+    % io:format("~p~n", [Items]),
+    lists:foreach(fun(Item) -> 
+                        io:format("-----------~n"),
+                        io:format("URL: ~p~n", [xmerl_xpath:string("//link/text()", Item)]),
+                        io:format("TIME: ~p~n", [xmerl_xpath:string("//pubDate/text()", Item)]),
+                        io:format("DESCRIPTION: ~p~n", [xmerl_xpath:string("//description/text()", Item)]),
+
+                        case xmerl_xpath:string("//author/text()", Item) of
+                            [] -> 
+                                io:format("AUTHOR: ~p~n", [Platform]);
+                            _ ->
+                                io:format("AUTHOR: ~p~n", [xmerl_xpath:string("//author/text()", Item)])
+                        end
+
+        end, Items),
+
     self() ! perform,
     {noreply, #spider_state{working_status=enabled, cooldown = Ms, url=Url, action = success}};
 
